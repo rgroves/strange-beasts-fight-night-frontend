6c685df48251c2df2e0bd63d31861a276cc4ecb8
Robert Groves <rwgdev@gmail.com>
2025-05-29 19:22:35 -0500

    Add exit game link

--- src/components/EndGamePanel.tsx
+++ src/components/EndGamePanel.tsx
@@ -6,19 +6,22 @@ interface EndGamePanelProps {
 
 export default function EndGamePanel({ gameState }: EndGamePanelProps) {
 	return (
-		<div className="end-game-panel">
-			{Object.values(gameState?.monsterImageMap).map((image) => (
-				<img
-					key={image}
-					height="400"
-					width="400"
-					src={`http://localhost:3000/static/${image}`}
-				/>
-			))}
-			<audio
-				controls
-				src={`http://localhost:3000/static/${gameState?.battleAudioFileName}`}
-			></audio>
-		</div>
+		<>
+			<div className="end-game-panel">
+				{Object.values(gameState?.monsterImageMap).map((image) => (
+					<img
+						key={image}
+						height="400"
+						width="400"
+						src={`http://localhost:3000/static/${image}`}
+					/>
+				))}
+				<audio
+					controls
+					src={`http://localhost:3000/static/${gameState?.battleAudioFileName}`}
+				></audio>
+			</div>
+			<a href="/">Exit</a>
+		</>
 	);
 }
